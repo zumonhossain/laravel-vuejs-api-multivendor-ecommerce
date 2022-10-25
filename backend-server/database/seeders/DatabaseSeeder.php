4b543df923b2102e4d4c39e9abf192ac95dd8c43
zumonhossain <zumon15-13169@diu.edu.bd>
2022-10-25 22:58:48 +0600

    Sub Category Model, Migration & Factory Create

--- backend-server/database/seeders/DatabaseSeeder.php
+++ backend-server/database/seeders/DatabaseSeeder.php
@@ -23,6 +23,7 @@ class DatabaseSeeder extends Seeder
         \App\Models\User::factory(10)->create();
         \App\Models\Brand::factory(20)->create();
         \App\Models\Category::factory(30)->create();
+        \App\Models\SubCategory::factory(100)->create();
 
 
         \App\Models\User::factory()->create([
