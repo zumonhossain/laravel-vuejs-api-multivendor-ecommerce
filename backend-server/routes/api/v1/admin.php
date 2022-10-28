1c12b70d3c3a74749c9a234fb0ab5d27ef251d16
zumonhossain <zumon15-13169@diu.edu.bd>
2022-10-28 21:13:23 +0600

    all api routes create

--- backend-server/routes/api/v1/admin.php
+++ backend-server/routes/api/v1/admin.php
@@ -2,8 +2,12 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\Admin\AdminAuthController;
-
-
+use App\Http\Controllers\Api\Admin\SliderController;
+use App\Http\Controllers\Api\Admin\BrandController;
+use App\Http\Controllers\Api\Admin\CategoryController;
+use App\Http\Controllers\Api\Admin\SubCategoryController;
+use App\Http\Controllers\Api\Admin\ProductController;
+use App\Http\Controllers\Api\Admin\DivisionController;
 
 Route::controller(AdminAuthController::class)->group(function(){
 
@@ -18,5 +22,15 @@ Route::middleware('auth:admin-api')->group(function () {
         Route::get('/me', 'user');
         Route::post('/logout', 'logout');
     });
+
+    Route::apiResources([
+        'sliders' => SliderController::class,
+        'brands' => BrandController::class,
+        'categories' => CategoryController::class,
+        'subcategories' => SubCategoryController::class,
+        'products' => ProductController::class,
+        'divisions' => DivisionController::class,
+    ]);
+
 
 });
